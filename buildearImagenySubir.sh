bc3f6376d5d39ae83d686b0c6e8cd67ac1b11081
korah91 <joel.upv@gmail.com>
2022-11-30 16:18:49 +0100

    ahora funciona la interfaz en 8080

--- buildearImagenySubir.sh
+++ buildearImagenySubir.sh
@@ -1,2 +1,6 @@
-docker build -t "korah91/rethinkdbscript:latest" .
-docker push "korah91/rethinkdbscript:latest"
+#docker build -t "korah91/rethinkdbscript:latest" .
+#docker push "korah91/rethinkdbscript:latest"
+
+docker build -t "korah91/nginx" -f Dockerfile.nginx --no-cache .
+docker push korah91/nginx
+./reiniciarTodo.sh
---
+++ inicializarTodo.sh
@@ -0,0 +1,23 @@
+# Este script inicializa todos los servicios, deployments y volumenes
+
+kubectl delete deployment server-rethinkdb
+kubectl delete deployment script
+kubectl delete deployment nginx
+kubectl delete pvc volumen-reclamacion
+
+# Servicio para acceder por IPTUNNELMINIKUBE:8080 a la interfaz grafica de rethinkDB
+kubectl apply -f service-rethinkdb.yml
+# Servicio para acceder por IPTUNNELMINIKUBE:80 a NGINX y ver los contenidos del volumen persistente
+kubectl apply -f service-nginx.yml
+
+
+# ClusterIP para que script cree las tablas en servidor-rethinkdb
+kubectl apply -f c-rethinkdb.yml
+# Creacion de reclamacion de volumen
+kubectl apply -f volumen-reclamacion.yml
+# Creacion de deployment de la base de datos rethinkDB 
+kubectl apply -f deployment-rethinkdb.yml
+# Creacion de deployment de container cliente
+kubectl apply -f deployment-script.yml
+# Creacion de deployment de servidor nginx que muestra el contenido del volumen.
+kubectl apply -f deployment-nginx.yml
