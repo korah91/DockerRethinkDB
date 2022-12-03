0dc0d72b168351e3259ba0506d9e87d2ab19820c
korah91 <joel.upv@gmail.com>
2022-12-03 16:18:25 +0100

    .

--- inicializarTodo.sh
+++ inicializarTodo.sh
@@ -1,8 +1,17 @@
 # Este script inicializa todos los servicios, deployments y volumenes
-# Se asume que minikube esta instalado y en marcha con el driver para ingress
+# Se asume que minikube esta instalado y en marcha con el driver para ingress: minikube addons enable ingress
 
 echo "Iniciando el cluster"
 
+# Un Namespace es un cluster virtual dentro del cluster fisico
+# Para desarrollo es buena idea utilizar un Namespace dev para desarrollo y prod para produccion
+echo "Elige el namespace a utilizar."
+read -p "Escribe dev o prod => " namesp
+
+# Se crea el namespace
+kubectl create namespace $namesp
+# Se configura el namespace que va a utilizar kubectl para sus instrucciones
+kubectl config set-context --current --namespace=$namesp
 kubectl delete deployment server-rethinkdb 
 kubectl delete deployment script 
 kubectl delete deployment nginx 
@@ -29,6 +38,12 @@ kubectl apply -f deployment-script.yml
 kubectl apply -f deployment-nginx.yml
 
 
+echo -e "Es normal que de errores de tipo not found\n"
+
+echo "El volumen persistente lo utiliza rethinkdb y nginx."
+podnginx=`kubectl get pods | grep nginx | cut -d ' ' -f1`
+echo -e "Ejecutando el comando 'kubectl exec -it ${podnginx} sh' y navegando a /usr/share/nginx/html/rethinkdb se encuentra el volumen montado con los datos de rethinkdb\n"
+
 # Se consigue la ip de minikube para acceder
 ip=`minikube ip`
-echo "Se accede a nginx en ${ip}"
+echo -e "Se accede a nginx en ${ip}\n"
