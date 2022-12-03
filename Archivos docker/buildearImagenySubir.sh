f20323e30839675146c42c73a751b1f0908a4ea0
korah91 <joel.upv@gmail.com>
2022-12-03 18:09:55 +0100

    .

--- Archivos docker/buildearImagenySubir.sh
+++ Archivos docker/buildearImagenySubir.sh
@@ -1,6 +1,5 @@
-#docker build -t "korah91/rethinkdbscript:latest" .
-#docker push "korah91/rethinkdbscript:latest"
+docker build -t "korah91/rethinkdbscript-docker:latest" .
+docker push "korah91/rethinkdbscript-docker:latest"
 
-docker build -t "korah91/nginx" -f Dockerfile.nginx --no-cache .
-docker push korah91/nginx
-./reiniciarTodo.sh
+#docker build -t "korah91/nginx" -f Dockerfile.nginx --no-cache .
+#docker push korah91/nginx
